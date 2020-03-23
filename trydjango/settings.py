c8484bfcdd4d2e1e49b8843e04c7aa0d0871d740
Dale Hamilton <z5115632@unsw.edu.au>
2020-03-23 12:49:28 +1100

    generic views, highcharts

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'products',
-    'polls.apps.PollsConfig'
+    'polls.apps.PollsConfig',
+    'highcharts'
 ]
 
 MIDDLEWARE = [
